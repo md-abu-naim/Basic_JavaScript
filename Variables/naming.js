a3e342537e95c4899e9a988a8e3be9f3e7b8ddfb
abunaim34 <farhanadnanfarabi@gmail.com>
2025-12-29 16:25:45 +0600

    Variable Namming

---
+++ Variables/naming.js
@@ -0,0 +1,29 @@
+// ******************************
+//  Lerning Variables Naming     //
+// ******************************
+
+// *********** Shoud be not use This ************
+// 1. no keyword in variable name
+var false = 10
+var var = 'string'
+
+// 2. No space & gap in Variable
+var is happy = true;
+
+
+// No qoute in Variables
+var "name" = "jamiya";
+
+
+// Can not start with a number, but number allowed in the last
+var 85 = false;
+
+
+
+
+// *********** Case sansative in the name ************
+
+var my current name = 'abu naim'    // not use this
+var my_current_name = 'abu naim'    // Snake Case
+var myCurrentName = 'abu naim'    // Camel Case
+var MyCurrentName = 'abu naim'    // Pascal Case
