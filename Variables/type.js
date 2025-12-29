5bbf8ddb2d02be4bf50d3537b9488b87717f162f
abunaim34 <farhanadnanfarabi@gmail.com>
2025-12-29 16:09:36 +0600

    Variable Types

---
+++ Variables/type.js
@@ -0,0 +1,34 @@
+// ******************************
+//  Lerning Variables Types     //
+// ******************************
+
+// Number Variable
+var weight = 50;
+var price = 10
+
+console.log(weight);
+
+
+// String Variable
+var name = "Abu Naim"
+var address = 'khajuria Senbag'
+console.log(name);
+
+
+// Boolean
+var isRich = false
+var passed = true
+
+console.log(isRich);
+console.log(passed);
+
+
+
+// Type Checking
+var price = 10
+var name = "Abu Naim"
+var isRich = false
+
+console.log(typeof price); // This is Number
+console.log(typeof name);  // This is String
+console.log(typeof isRich);//This is Boolean
