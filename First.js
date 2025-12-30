1100c97853bb6ee35807f102151aa4d223341bd7
abunaim34 <farhanadnanfarabi@gmail.com>
2025-12-30 15:51:38 +0600

    short hand variables

--- First.js
+++ First.js
@@ -4,3 +4,10 @@
 
 console.log(85);
 console.log('object');
+
+
+
+
+// accha reels er kaj ta ki vabe kore mane dhoro facebook e jkhn amr ba karo profile e duke reels page theke ekta reels click kori tkhn to sudhu oi id er sob gula reels ektar por ekta dekhay ... 
+
+// but profile e na giye navber e jkhn reels page duki tokhn to R ekjorer reels ase na vivinno joner reels ase hajaro profile theke seta ki vabe kore bistarito bujhiye bolo
---
+++ Variables/shortHand.js
@@ -0,0 +1,28 @@
+// ******************************************
+//  Lerning short hand Variable declare     //
+// ******************************************
+
+// Sum
+var price = 10
+price = price + 10
+price += 10 // short hand
+
+console.log(price);
+
+
+// Minus
+var frigd = 3500;
+frigd = frigd - 1000
+frigd -= 500 // short hand
+
+console.log(frigd);
+
+
+// Multiply
+var orange = 50
+orange = orange * 3
+orange *= 3 // short hand
+
+console.log(orange);
+
+
