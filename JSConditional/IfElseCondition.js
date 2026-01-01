2d0af06668f01df0286515b37242ac9ba02b083f
abunaim34 <farhanadnanfarabi@gmail.com>
2026-01-01 15:33:27 +0600

    if else condition

---
+++ JSConditional/IfElseCondition.js
@@ -0,0 +1,38 @@
+// ******************************************
+//            Lerning if else conditions    //
+// ******************************************
+
+if(3 < 50){
+    console.log('I have got a small letter'); // find output
+}
+
+// With variable
+var biriyani = 500
+
+if(biriyani < 300) {
+    console.log('give me some biriyani'); // not output
+}
+
+if(biriyani > 300) {
+    console.log('I find biriyani'); // find output
+}
+
+
+// if condition with else{}
+var weight = 40
+
+if(weight < 20) {
+    console.log('I will carry it');
+}
+else{
+    console.log('I will rent a rikshaw'); // find it
+}
+
+
+if(weight > 20) {
+    console.log('I will carry it'); // find it
+}
+else{
+    console.log('I will rent a rikshaw');
+}
+ 
