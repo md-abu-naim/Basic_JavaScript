a9c4b27850f8cf0abba1459283341a927eb2616c
abunaim34 <farhanadnanfarabi@gmail.com>
2026-01-04 14:55:16 +0600

    Learn array

---
+++ Array/array.js
@@ -0,0 +1,9 @@
+// ******************************************
+//          JavaScript Lerning Array       //
+// ******************************************
+
+
+const numbers = [10, 50, 47, 1, 52,] // this is numbers array
+const strings = ['Abul', 'Babul', 'Kabul', 'Bulbul'] // this is string array
+const boolean = [false, true, false, true]  // this is boolean array
+const mixed = [10, 'Norail', false, 522, true]
