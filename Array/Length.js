5945ad410130b8320403811a0ed4767a9b09add0
abunaim34 <farhanadnanfarabi@gmail.com>
2026-01-04 15:00:18 +0600

    Array length

---
+++ Array/Length.js
@@ -0,0 +1,7 @@
+// ******************************************
+//     JavaScript Lerning Array Length     //
+// ******************************************
+
+const numbers = [10, 85, 18, 22, 85, 25, 244, 7455, 44, 88, 99]
+
+console.log(numbers.length); // Length 11
