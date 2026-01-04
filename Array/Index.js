7dac2e0a5e095e3f8da4abd62cab48814dab76db
abunaim34 <farhanadnanfarabi@gmail.com>
2026-01-04 15:10:18 +0600

    Array index

---
+++ Array/Index.js
@@ -0,0 +1,22 @@
+// ******************************************
+//        JavaScript Lerning Index         //
+// ******************************************
+
+const numbers = [10, 85, 18, 22, 85, 25, 244, 7455, 44, 88, 99]
+
+console.log(numbers); // [10, 85, 18, 22, 85, 25, 244, 7455, 44, 88, 99]
+console.log(numbers[0]); // 10
+console.log(numbers[3]); // 22
+
+
+// You can set index in variable
+const nine = numbers[8]
+
+console.log(nine); // 44
+
+
+
+// You can set or update array index
+numbers[1] = 999
+
+console.log(numbers);  // [10, 85, 18, 22, 85, 25, 244, 7455, 44, 88, 99]
