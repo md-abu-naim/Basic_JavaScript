6f41e5a1335bfd02c61d2c3b079331a309e0077f
abunaim34 <farhanadnanfarabi@gmail.com>
2026-02-08 15:25:52 +0600

    indeOf() using for search index in array

---
+++ Array/IndexOf.js
@@ -0,0 +1,9 @@
+// ***************************************************
+// JavaScript Lerning indexOf() for search index //
+// ***************************************************
+
+const friends = ['kalam', 'salam', 'balam', 'khailam' ]
+console.log(friends); // ['kalam', 'salam', 'balam', 'khailam' ]
+
+console.log(friends.indexOf('balam')); // 2
+console.log(friends.indexOf('naim')); // -1 bcs it's none in array
