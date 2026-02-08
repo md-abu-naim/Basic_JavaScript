e98b64d68e9e9326eac5da0e6c5d741d41257d17
abunaim34 <farhanadnanfarabi@gmail.com>
2026-02-08 15:01:48 +0600

    unshift() for add index at the first array

---
+++ Array/Unshift.js
@@ -0,0 +1,9 @@
+// ***************************************************
+// JavaScript Lerning Unhift() for add first index //
+// ***************************************************
+
+const friends = ['salam', 'balam', 'khailam' ]
+console.log(friends); // ['salam', 'balam', 'khailam' ]
+
+friends.unshift('kalam')
+console.log(friends); // ['kalam', 'salam', 'balam', 'khailam' ]
