1a86995741b4110a1766ae652b95c011636e4641
abunaim34 <farhanadnanfarabi@gmail.com>
2026-02-08 15:35:55 +0600

    join() using for join array

---
+++ Array/Join.js
@@ -0,0 +1,11 @@
+// ***************************************************
+// JavaScript Lerning join() for join index //
+// ***************************************************
+
+const friends = [ 'kalam', 'salam', 'balam', 'khailam' ]
+console.log(friends); // [ 'kalam', 'salam', 'balam', 'khailam' ]
+
+console.log(friends.join()); // kalam, salam, balam, khailam
+console.log(friends.join('')); // kalamsalambalamkhailam
+console.log(friends.join('+')); // kalam+salam+balam+khailam
+console.log(friends.join('|')); // kalam|salam|balam|khailam
