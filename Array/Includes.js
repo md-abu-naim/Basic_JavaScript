855659c0d5f7dc1ffc97c0f967aa07a24b2ee7fa
abunaim34 <farhanadnanfarabi@gmail.com>
2026-02-08 15:21:24 +0600

    includes() for search index

---
+++ Array/Includes.js
@@ -0,0 +1,9 @@
+// ***************************************************
+// JavaScript Lerning includes() for search an index //
+// ***************************************************
+
+const friends = [ 'kalam', 'salam', 'balam', 'khailam' ]
+friends.includes('khailam')
+
+console.log(friends.includes('khailam')); // true
+console.log(friends.includes('Khailam')); // false bcz case sensetive
