a4ca7bd7532c1722cbd16010260b31671c331399
abunaim34 <farhanadnanfarabi@gmail.com>
2026-02-08 15:43:14 +0600

    slice() using for remove index from to from

---
+++ Array/Slice.js
@@ -0,0 +1,8 @@
+// ***************************************************
+// JavaScript Lerning slice() for add first index //
+// ***************************************************
+
+const nums = [10, 21, 85, 72]
+console.log(nums); // [ 10, 21, 85, 72 ]
+
+console.log(nums.slice(0, 2)); // [ 10, 21,]
