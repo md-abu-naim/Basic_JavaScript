1b7284feb6425c0b582b38a3f3e410b3fbab3a89
abunaim34 <farhanadnanfarabi@gmail.com>
2026-02-08 15:38:53 +0600

    concate() using for join two array in one array

---
+++ Array/Concate.js
@@ -0,0 +1,8 @@
+// ***************************************************
+// JavaScript Lerning concate() for to array in one array //
+// ***************************************************
+
+const friends = ['salam', 'balam', 'khailam' ]
+const num = ['abul', 'babul', 'cabul' ]
+
+console.log(friends.concat(num)); // [ 'salam', 'balam', 'khailam', 'abul', 'babul', 'cabul' ]
