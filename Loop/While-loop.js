be035e56775c83e52ca336969ef43112d3f52b9f
abunaim34 <farhanadnanfarabi@gmail.com>
2026-02-11 21:13:33 +0600

    get even numbers using while loop

--- Loop/While-loop.js
+++ Loop/While-loop.js
@@ -15,12 +15,34 @@ while(sum <= 30){
     sum++   // it's must be do
 }
 
-// sum 
+// Get sum number
 let num1 = 1
 let total = 0
 while(num1 <= 10){
-    console.log(num1);
+    // console.log(num1);
     total = total + num1
-    console.log('sum', total );
+    // console.log('sum', total );
     num1++
 }
+
+
+// Get Even number
+let num2 = 1
+while(num2 <= 10){
+    // console.log(num2);
+
+    if(num2 % 2 === 0){
+        // console.log('even number', num2);
+    }
+    num2++
+}
+
+
+// Get odd numbers
+// let num3 = 1
+// while(num3 <= 10){
+//     if(num3 % 2 === 1){
+//         console.log(num3);
+//     }
+//     num3++
+// }
