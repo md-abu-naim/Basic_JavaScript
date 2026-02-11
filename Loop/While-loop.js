4952e52a3f066f8f8ebe69244da58d2fa4fb7fa5
abunaim34 <farhanadnanfarabi@gmail.com>
2026-02-11 21:08:01 +0600

    get sum using while loop

--- Loop/While-loop.js
+++ Loop/While-loop.js
@@ -5,12 +5,22 @@
 
 let num = 1
 while(num <= 10){
-    console.log(num);
+    // console.log(num);
     num += 1   // it's must be do
 }
 
 let sum  = 10
 while(sum <= 30){
-    console.log(sum);
+    // console.log(sum);
     sum++   // it's must be do
+}
+
+// sum 
+let num1 = 1
+let total = 0
+while(num1 <= 10){
+    console.log(num1);
+    total = total + num1
+    console.log('sum', total );
+    num1++
 }
