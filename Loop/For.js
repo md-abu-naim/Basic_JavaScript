0cfd60bd6a2d0fb90ecdf8a531e07202be4d9876
abunaim34 <farhanadnanfarabi@gmail.com>
2026-02-11 22:06:44 +0600

    get odd numbers using for-loop

--- Loop/For.js
+++ Loop/For.js
@@ -3,5 +3,29 @@
 // ******************************************
 
 for(let num = 0; num <= 5; num++){
-    console.log(num);
+    // console.log(num);
 }
+
+
+// Odd Numbers
+for(let i = 0; i <= 5; i++){
+    if(i % 2 === 0){
+        // console.log(i);
+    }
+}
+
+
+// Even Numbers
+// for(let i = 0; i <= 5; i++){
+//     if(i % 2 === 1){
+//         // console.log(i);
+//     }
+// }
+
+
+// // Sum numbers
+// let sum = 0
+// for(let i = 51; i <= 101; i++){
+//     sum = sum + i
+//     console.log(i, sum);
+// } 
