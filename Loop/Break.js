0638ef0b52f7de96e3e53a5ef525fd029e4b7d1b
abunaim34 <farhanadnanfarabi@gmail.com>
2026-02-15 19:26:08 +0600

    break using while loop

--- Loop/Break.js
+++ Loop/Break.js
@@ -2,9 +2,21 @@
 //      JavaScript Lerning for loop      //
 // ******************************************
 
+// break using for loop
 for (let i = 0; i <= 150; i++){
-    console.log(i);
+    // console.log(i);
     if(i >= 5){
         break
     }
+}
+
+
+// break using while loop
+let num = 55
+while(num > 20){
+    console.log(num);
+    if(num < 50){
+        break
+    }
+    num--
 }
