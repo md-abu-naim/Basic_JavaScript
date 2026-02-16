143421ed28cf0ded448eebe5e7738dbab84b59de
abunaim34 <farhanadnanfarabi@gmail.com>
2026-02-16 19:07:47 +0600

    continue in for while loop

--- Loop/Continue.js
+++ Loop/Continue.js
@@ -9,11 +9,11 @@ for(let i = 1; i <= 10; i++){
     // console.log(i);
 }
 
-// let i = 0
-// while(i < 50){
-//     if(i % 5 !== 0) {
-//         continue
-//     }
-//     console.log(i);
-//     i++
-// }
+let i = 0
+while(i < 50){
+    i++
+    if(i % 5 !== 0) {
+        continue
+    }
+    console.log(i);
+}
