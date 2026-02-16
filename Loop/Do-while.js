bf69b6c1a6a0e693ae191aba5b3afb1ff4e5ac28
abunaim34 <farhanadnanfarabi@gmail.com>
2026-02-16 19:15:28 +0600

    learning do while loop

---
+++ Loop/Do-while.js
@@ -0,0 +1,18 @@
+// ******************************************
+//      JavaScript Lerning do while loop      //
+// ******************************************
+
+// while loop
+let i = 0
+while(i < 10){
+    // console.log(i);
+    i++
+}
+
+// do while loop
+let n = 0
+do{
+    console.log(0);
+    n++
+}
+while(n < 10)
