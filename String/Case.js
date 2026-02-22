088b86e790c5f9d551296507f87222407b233b9a
abunaim34 <farhanadnanfarabi@gmail.com>
2026-02-22 21:46:40 +0600

    Learned String toLowerCase() & toUpperCase()

---
+++ String/Case.js
@@ -0,0 +1,28 @@
+
+
+const madrasa = 'Jamiya Mohammadiya Khajuria Madrash'
+console.log(madrasa);               // Jamiya Mohammadiya Khajuria Madrash
+console.log(madrasa.toLowerCase()); // jamiya mohammadiya khajuria madrash
+console.log(madrasa.toUpperCase()); // JAMIYA MOHAMMADIYA KHAJURIA MADRASH
+
+
+const book = 'hedaya'
+const subject = 'Hedaya'
+
+if(book === subject){
+    console.log('its okk');
+}else{
+    console.log('its not okk'); // its not okk
+}
+
+if(book.toLowerCase() === subject.toLowerCase()){
+    console.log('its okkk'); // its okk
+}else{
+    console.log('its not okk');
+}
+
+if(book.toUpperCase() === subject.toUpperCase()){
+    console.log('its okkk'); // its okk
+}else{
+    console.log('its not okk');
+}
