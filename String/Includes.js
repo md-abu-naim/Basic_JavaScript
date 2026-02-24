461f4248356858c096c2b8704c6a2de43d2bcecf
abunaim34 <farhanadnanfarabi@gmail.com>
2026-02-24 21:59:54 +0600

    learingn includes

---
+++ String/Includes.js
@@ -0,0 +1,7 @@
+// learning includes method in string
+
+const sentence = 'I am s student of programming'
+
+console.log(sentence.includes('a')); // true
+console.log(sentence.includes('student')); // true
+console.log(sentence.includes('sami')); // false
