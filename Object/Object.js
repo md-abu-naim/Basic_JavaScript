10dde4296be427736fafd4d63dbb005800c910d9
abunaim34 <farhanadnanfarabi@gmail.com>
2026-02-25 21:48:41 +0600

    learning object

---
+++ Object/Object.js
@@ -0,0 +1,22 @@
+// Learing Object in JavaScript
+// Object is a collection of properties, and a property is an association between a name (or key) and a value.
+
+
+const person = {
+    name: 'Naim',
+    age: '23',
+    city: 'Dhaka',
+    profession: 'Web Developer'
+}
+
+
+const subject = {
+    name: 'Hedaya',
+    teacher: 'Jakir Hossain',
+    examDate: '2024-07-01',
+    topics: ['HTML', 'CSS', 'JavaScript'],
+    subject: {
+        name: 'Programming',
+        code: 'CS101',
+    }
+}
