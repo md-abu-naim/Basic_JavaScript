1871d8519b238095458bb0d08f5d3687ca311cb6
abunaim34 <farhanadnanfarabi@gmail.com>
2026-02-27 22:16:41 +0600

    learn nested  Object

--- Object/Declare.js
+++ Object/Declare.js
@@ -1,12 +1,12 @@
 // option 1
-const pen = {brand: 'metador', price: 10, color: 'black'}
+const pen = { brand: 'metador', price: 10, color: 'black' }
 console.log(pen); // {brand: 'metador', price: 10, color: 'black'}
 
 // option 2
-const pencil = new Object({name: 'ahmed'})
+const pencil = new Object({ name: 'ahmed' })
 console.log(pencil); // {name: 'ahmed'}
 
 
 // option 3
-const rubber = Object.create({name: 'kalam'})
+const rubber = Object.create({ name: 'kalam' })
 console.log(rubber); // {name: 'kalam'}
--- Object/For-in-loop.js
+++ Object/For-in-loop.js
@@ -5,7 +5,7 @@ const mobile = {
     isNew: false
 }
 
-for(const prop in mobile){
+for (const prop in mobile) {
     console.log(prop); // brand, price, color, isNew
     console.log(mobile[prop]); // brand: apple, price: 27000, color: white, isNew: false
 }
@@ -14,7 +14,7 @@ for(const prop in mobile){
 const keys = Object.keys(mobile)
 console.log(keys); // [ 'brand', 'price', 'color', 'isNew' ]
 
-for(const key of keys){
+for (const key of keys) {
     console.log(key); // brand, price, color, isNew
     console.log(mobile[key]); // // brand: apple, price: 27000, color: white, isNew: false
 }
