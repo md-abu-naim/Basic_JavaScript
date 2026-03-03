566c8b032b014e6e39d72920ffa858de71a1ae79
abunaim34 <farhanadnanfarabi@gmail.com>
2026-03-03 22:27:49 +0600

    learing parameter in function

---
+++ Function/Parameter.js
@@ -0,0 +1,26 @@
+
+function squar(numbers){
+    console.log(numbers);
+    const borgo = numbers * numbers
+    console.log(borgo);
+}
+
+// squar(4) // 14
+// squar(11) // 121
+
+
+function add(num1, num2){
+    const sum = num1 + num2
+    console.log(sum);
+}
+
+// add(52, 85) // 137
+// add(5, 10) // 15
+
+
+function addAll(a, b, c, d,){
+    const sum = a + b + c + d
+    console.log(sum);
+}
+
+addAll(4, 5, 7, 8) // 24
