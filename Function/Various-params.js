190b9003b2bc6596066d16d66100fdbcf21daf47
abunaim34 <farhanadnanfarabi@gmail.com>
2026-03-05 21:50:12 +0600

    argument can be arry

--- Function/Various-params.js
+++ Function/Various-params.js
@@ -2,7 +2,7 @@ function stringSized(str){
     const size = str.length
     return size
 }
-console.log(stringSized('Noakhali')); // 8
+// console.log(stringSized('Noakhali')); // 8
 
 
 function doubleOrTriple(num, blen){
@@ -13,5 +13,13 @@ function doubleOrTriple(num, blen){
     }
 }
 
-console.log(doubleOrTriple(2, false)); // 10
-console.log(doubleOrTriple(2, true)); // 6
+// console.log(doubleOrTriple(2, false)); // 10
+// console.log(doubleOrTriple(2, true)); // 6
+
+
+function numberOfElements(arr){
+    const len = arr.length
+    return len
+}
+
+console.log(numberOfElements([52, 55, 74, 52])); // 4
