f7a90904247c43cc927feade17c7af38076faa4b
abunaim34 <farhanadnanfarabi@gmail.com>
2026-03-05 21:26:19 +0600

    function retun short hand

--- Function/Return.js
+++ Function/Return.js
@@ -9,3 +9,13 @@ console.log(output);
 
 const bigNumber = tenTimes(1000)
 console.log(bigNumber);
+
+
+function doMath(num, num2){
+    const sum = num + num2
+    const diff = sum - num2
+    return diff
+}
+
+const bill = doMath(10, 51)
+console.log(bill);
