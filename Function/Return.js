26d90671491ea57b36434f53b9d113400fce234c
abunaim34 <farhanadnanfarabi@gmail.com>
2026-03-05 21:19:26 +0600

    function can return & save the value in a variable

---
+++ Function/Return.js
@@ -0,0 +1,11 @@
+function tenTimes(number){
+    const ten = number * 10
+    return ten
+}
+
+const output = tenTimes(50)
+console.log(output);
+
+
+const bigNumber = tenTimes(1000)
+console.log(bigNumber);
