d85d1595530bb8779206c4eb290c595f1547b048
abunaim34 <farhanadnanfarabi@gmail.com>
2026-03-06 21:28:07 +0600

    Day7 practice task ==> task 5

--- PracticeTask/Day7.js
+++ PracticeTask/Day7.js
@@ -65,4 +65,21 @@ function count_zero(strs){
     return count
 }
 
-console.log(count_zero('00100101'));
+// console.log(count_zero('00100101'));
+
+
+/**
+ * Task-5
+Write a function called odd_even() which takes an integer value and tells whether this value is even or odd. If even return Even. If odd return Odd
+ */
+
+function odd_even(num){
+    if(num % 2 == 0){
+        return 'Even'
+    }else{
+        return 'Odd' 
+    }
+}
+
+console.log(odd_even(3));
+console.log(odd_even(4));
