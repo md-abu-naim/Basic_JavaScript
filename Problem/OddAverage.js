f6a0944ba4f57c91e42b06cb15bcc45c0d38dd84
abunaim34 <farhanadnanfarabi@gmail.com>
2026-03-13 22:05:15 +0600

    calculate-the-average-of-the-odd-numbers-in-an-array

---
+++ Problem/OddAverage.js
@@ -0,0 +1,20 @@
+function oddAverage(numbers){
+    const odds = []
+    let sum = 0
+    for(const odd of numbers){
+        if(odd % 2 === 1){
+            odds.push(odd)
+        }
+    }
+    
+    for(const number of odds){
+        sum = sum + number
+    }
+
+    const length = odds.length
+    const average = sum / length
+    return average
+}
+
+const numbers = [51, 41, 66, 88, 33, 1]
+console.log(oddAverage(numbers)); // 31.5
