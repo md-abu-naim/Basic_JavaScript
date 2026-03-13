411c8e08410bf56de191736902f596cfd882fd94
abunaim34 <farhanadnanfarabi@gmail.com>
2026-03-13 21:53:38 +0600

    check whether a year is a leap year or not

--- Problem/LeapYear.js
+++ Problem/LeapYear.js
@@ -5,5 +5,20 @@ function isLeapYear(year){
     return false
 }
 
-console.log(isLeapYear(2026)); // ture
-console.log(isLeapYear(2028)); // false
+// console.log(isLeapYear(2026)); // false
+// console.log(isLeapYear(2028)); // true
+
+
+
+function isLeapYear2(year){
+    if(year % 100 !== 0 && year % 4 === 0){
+        return true
+    }
+    if(year % 400 === 0){
+        return true
+    }
+    return false
+}
+
+console.log(isLeapYear(2026)); // false
+console.log(isLeapYear(2072)); // true
