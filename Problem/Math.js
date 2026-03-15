9f542a6bd3c6eaba07a0a163a271413f53f6069c
abunaim34 <farhanadnanfarabi@gmail.com>
2026-03-15 22:27:54 +0600

    use js math.max

--- Problem/Math.js
+++ Problem/Math.js
@@ -1,3 +1,8 @@
 // min number
 const min = Math.min(41, 52, 4, 88, 7155, 552)
-console.log(min);
+// console.log(min); // 4
+
+
+// Max number
+const max = Math.max(41, 52, 4, 88, 7155, 552)
+console.log(max); // 7155
