e854e4dc042aab6901ecd40d3ddc2f9f5f712cd3
abunaim34 <farhanadnanfarabi@gmail.com>
2026-03-18 21:24:33 +0600

    learn Math.abs()

--- Problem/Math.js
+++ Problem/Math.js
@@ -8,4 +8,7 @@ const max = Math.max(41, 52, 4, 88, 7155, 552)
 // console.log(max); // 7155
 
 
-console.log(Math.PI); // 3.141592653589793
+// console.log(Math.PI); // 3.141592653589793
+
+// Deffrent between tow numbers 
+console.log(Math.abs(10 - 25)); // 15
