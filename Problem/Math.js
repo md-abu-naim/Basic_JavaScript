cc4f4bb1174a1602bf207e9a1118d7278e5db4c1
abunaim34 <farhanadnanfarabi@gmail.com>
2026-03-18 21:27:20 +0600

    learn Math.round()

--- Problem/Math.js
+++ Problem/Math.js
@@ -11,4 +11,8 @@ const max = Math.max(41, 52, 4, 88, 7155, 552)
 // console.log(Math.PI); // 3.141592653589793
 
 // Deffrent between tow numbers 
-console.log(Math.abs(10 - 25)); // 15
+// console.log(Math.abs(10 - 25)); // 15
+
+// Get full 
+// console.log(Math.round(2.25)); // 2
+// console.log(Math.round(2.85)); // 3
