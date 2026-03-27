2c8e6e4381a7f05a3ef02c8d6ea58245b9fb44cc
abunaim34 <farhanadnanfarabi@gmail.com>
2026-03-27 20:45:48 +0600

    Day8 PracticeTask findLongestWord

--- PracticeTask/Day8.js
+++ PracticeTask/Day8.js
@@ -65,6 +65,19 @@ sample-input: I am learning Programming to become a programmer
 sample-output: Programming
  */
 
+function findLongestWord(str){
+    const words = str.split(' ')
+    let longestWord = ''
+
+    words.forEach(word => {
+        if(word.length > longestWord.length){
+            longestWord = word
+        }
+    })
+    return longestWord
+}
+
+console.log(findLongestWord('I am learning Programming to become a programmer'));
 
 
 /**
