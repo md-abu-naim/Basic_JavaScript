6e00cc06230e938af1f2ef43f1d021ae2060b420
abunaim34 <farhanadnanfarabi@gmail.com>
2026-03-27 20:19:14 +0600

    Day8 PracticeTask getVowelsInString

--- PracticeTask/Day8.js
+++ PracticeTask/Day8.js
@@ -8,4 +8,48 @@ function celsiusToFahrenheit(celsius){
     return fahrenheit
 }
 
-console.log(celsiusToFahrenheit(20));
+// console.log(celsiusToFahrenheit(20)); // 68
+
+
+/**
+ * Task-2:
+You are given an array of numbers. Count how many times the a number is repeated in the array.
+
+sample-input: numbers = [5,6,11,12,98, 5]
+
+find: 5
+
+output: 2
+
+
+sample-input:
+
+numbers = [5,6,11,12,98, 5]
+
+find: 25
+
+output: 0
+ */
+
+
+
+
+
+/**
+ * Task-3:
+Write a function to count the number of vowels in a string.
+ */
+
+function getVowels(str){
+    const vowels = 'aeiouAEIOU'
+    let count = 0 
+
+    for(const st of str){
+        if(vowels.includes(st)){
+            count++
+        }
+    }
+    return count
+}
+
+console.log(getVowels('akashbuio')); // 5
