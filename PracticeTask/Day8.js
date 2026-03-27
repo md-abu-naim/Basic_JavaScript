e7b09858f3d12fd2babe094e63eca005c0743f41
abunaim34 <farhanadnanfarabi@gmail.com>
2026-03-27 20:02:09 +0600

    Day8 PracticeTask celsiusToFahrenheit

---
+++ PracticeTask/Day8.js
@@ -0,0 +1,11 @@
+/**
+ * Task-1:
+Write a function to convert temperature from Celsius to Fahrenheit.
+ */
+
+function celsiusToFahrenheit(celsius){
+    const fahrenheit = (celsius * 9/5) + 32
+    return fahrenheit
+}
+
+console.log(celsiusToFahrenheit(20));
