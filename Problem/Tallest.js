adbbb26835b8e4d8ed62715322eb23364f636692
abunaim34 <farhanadnanfarabi@gmail.com>
2026-03-28 22:08:20 +0600

    Who is the tallest? Find the max number in an array

--- Problem/Tallest.js
+++ Problem/Tallest.js
@@ -1,9 +1,9 @@
 const heights = [55, 77, 85, 25, 69, 96, 100]
 
-function getMax(numbers){
+function getMax(numbers) {
     let max = numbers[0]
-    for(const num of numbers){
-        if(num > max) {
+    for (const num of numbers) {
+        if (num > max) {
             max = num
         }
     }
@@ -14,10 +14,10 @@ function getMax(numbers){
 
 
 
-function getMin(numbers){
+function getMin(numbers) {
     let min = numbers[0]
-    for(const num of numbers){
-        if(num < min){
+    for (const num of numbers) {
+        if (num < min) {
             min = num
         }
     }
